4ae4a31cd1bc8c08e748609222b25c0356a18366
goldentg <43283685+goldentg@users.noreply.github.com>
2020-05-22 18:48:01 -0600

    1.0 RELEASE

--- DOW GUI/Form1.cs
+++ DOW GUI/Form1.cs
@@ -7,6 +7,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using System.IO;
 
 namespace DOW_GUI
 {
@@ -32,7 +33,9 @@ namespace DOW_GUI
             } 
             else
             {
-
+                TextWriter txt = new StreamWriter("C:/DOW GUI/DOW GUI/bin/Debug/token.txt");
+                txt.Write(textBox1.Text);
+                txt.Close();
             }
         }
 
